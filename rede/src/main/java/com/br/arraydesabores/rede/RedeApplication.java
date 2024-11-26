d4e56f678d8eda80a80bedf16363141515691d57
Bryan Trajano <bryantrajano18@gmail.com>
2024-11-26 19:30:06 -0300

    Start Projeto

---
+++ rede/src/main/java/com/br/arraydesabores/rede/RedeApplication.java
@@ -0,0 +1,13 @@
+package com.br.arraydesabores.rede;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedeApplication.class, args);
+	}
+
+}
---
+++ rede/src/test/java/com/br/arraydesabores/rede/RedeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.arraydesabores.rede;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
